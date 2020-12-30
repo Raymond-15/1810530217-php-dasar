8b31a3c939a61d25254ca6c50e007558cf4fc4b7
Raymond-15 <raymondputra15@gmail.com>
2020-12-30 10:14:10 +0800

    #3

--- create.php
+++ create.php
@@ -1,9 +1,3 @@
-<?php 
-require 'functions.php';
-$mahasiswa = query("SELECT * FROM mahasiswa");
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,46 +8,33 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-
-
-	<title>Tabel Mahasiswa</title>
+	<title></title>
 </head>
 <body>
 	<div class="container">
-		<div class="alert alert-info"><h1>Tambah Data</h1></div>
+		<div class="alert alert-info">Tambah Data</div>
 
-		<a href="create.php" class="btn btn-primary">Tambah Data</a>
-		<br><br>
-
-		<table cellspacing="0" cellpadding="10" class="table align-middle table-bordered">
-			<tr>
-				<th>No.</th>
-				<th>NIM</th>
-				<th>Nama</th>
-				<th>Prodi</th>
-				<th>Aksi</th>
-			</tr>
-
-			<?php $i = 1; ?>
-			<?php foreach($mahasiswa as $row) :?>
-			<tr>
-				<td><?= $i; ?></td>
-				<td><?= $row["nim"]; ?></td>
-				<td><?= $row["nama_mahasiswa"]; ?></td>
-				<td><?= $row["prodi"]; ?></td>
-				<td>
-					<a href="" class="btn btn-danger">Hapus</a>
-					<a href="" class="btn btn-warning">Ubah</a>
-				</td>
-			</tr>
-			<?php $i++; ?>
-			<?php endforeach; ?>
-		</table>
+		<form action="" method="post">
+			<div class="form-group">
+				<label for="">NIM</label>
+				<input type="text" class="form-control" name="txtnim">
+			</div>
+			<div class="form-group">
+				<label for="">Nama</label>
+				<input type="text" class="form-control" name="txtnama">
+			</div>
+			<div class="form-group">
+				<label for="">Prodi</label>
+				<input type="text" class="form-control" name="txtprodi">
+			</div>
+			<br>
+			<input type="submit" class="btn btn-success" name="simpan" value="Simpan Data">
+			<a href="index.php" class="btn btn-warning">Batal</a>
+		</form>
 	</div>
+
 
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-   
-
 </body>
 </html>
